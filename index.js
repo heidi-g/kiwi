f746734a2658dda95c1b2134421a984303983232
James Sangalli <j.l.sangalli@gmail.com>
2016-04-15 11:10:36 +1200

    add hbs

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
+#!/usr/bin/env node
+var knex = require('knex')({
+  client: 'sqlite3',
+  connection: {
+    filename: __dirname + '/dev.sqlite3'
+  },
+  useNullAsDefault: true
+})
+
 var express = require('express')
 var app = express()
 var path = require("path")
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join());
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname));
 var fs = require("fs")
 var request = require("superagent")
-var randomNumber = Math.floor((Math.random() * 21) + 1)
 
+
+//-----select by ID -----
 function wordMatch(){
-  return knex.raw('SELECT FROM "todos" WHERE id=' + id + ';')
+  var randomNumber = Math.floor((Math.random() * 21) + 1)
+  return knex.raw('SELECT id,kiwiWord,expression FROM "kiwi" WHERE id=' + randomNumber + ';')
+}
+
+function logError (err) {
+  console.log('Dang, we exploded like a bomb: ', err)
+}
+
+function closeDB () {
+  knex.destroy()
 }
 
 app.listen(3000, function () {
   console.log('listening on port 3000!');
 });
 
-app.get("/v1/home",function(req,res){
-  res.send("HELLO")
+app.get("/",function(req,res){
+  res.render("index")
+});
+
+app.get("/home".function(){
+  res.render("index")
 });
