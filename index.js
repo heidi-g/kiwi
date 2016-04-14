f300c2eec27b154a5ea3e03ae649db65c8883b4d
James Sangalli <j.l.sangalli@gmail.com>
2016-04-14 18:13:53 +1200

    create basic files and package

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express')
+var app = express()
+var path = require("path")
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname));
+var fs = require("fs")
+var request = require("superagent")
+var randomNumber = Math.floor((Math.random() * 21) + 1)
+
+function wordMatch(){
+
+}
+
+app.listen(3000, function () {
+  console.log('listening on port 3000!');
+});
+
+app.get("/v1/home",function(req,res){
+  res.send("HELLO")
+});
